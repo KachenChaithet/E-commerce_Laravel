8db314861a436ccf5287475732ea5cb2ff3bae76
Kachen Chaithet <chaytheskhchenthr@gmail.com>
2026-01-04 11:54:42 +0700

    Add index view and update route to use new view
    - Created a new index.blade.php file for the homepage layout.
    - Updated the web.php route to return the new index view instead of the welcome view.

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,8 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('index');
 
 Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
